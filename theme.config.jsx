7cd8e2af8087c00ce8e351250433ff6f3c7ccfda
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2024-05-30 20:58:44 +0530

    Build || added Nextra working Good

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,30 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>NAS Js</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
-    }
+      link: 'https://github.com/raunaksingh9800/NAS-System'
+    },
+    footer: {
+        text: (
+          <span>
+            MIT {new Date().getFullYear()} ©{' '}
+            <a href="https://nextra.site" target="_blank">
+              NAS Js
+            </a>
+            .
+          </span>
+        )
+      },
+      headTitle: "NAS JS",
+      head: (
+        <>
+        <title>NAS Js Docs</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="This is a Documentation About NAS Js" />
+            <meta name="keywords" content="NAS Js , NAS Js Password , NAS Js Installation" />
+            <meta name="author" content="Raunak Singh" />
+        </>
+      )
+
+
     // ... other theme options
   }
