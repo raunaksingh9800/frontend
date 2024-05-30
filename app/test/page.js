370ff3beb790c38827d535ff8fc1a543e5828c82
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2024-05-30 22:10:00 +0530

    added more docs

--- app/test/page.js
+++ app/test/page.js
@@ -1,6 +1,19 @@
+import React from "react";
+import { FlipWords } from "./flip-words";
 
-export default function main() {
+export default function FlipWordsDemo() {
+  const words = ["better", "cute", "beautiful", "modern"];
+
   return (
-    <>h1</>
-  )
+    <div className="w-screen h-screen bg-black">
+    <div className="h-[40rem] flex justify-center items-center px-4">
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        Enjoy
+        <FlipWords words={words} duration={1000} /> <br />
+        With NAS Js
+      </div>
+      </div>
+
+    </div>
+  );
 }
