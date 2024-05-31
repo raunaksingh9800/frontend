6f0043e9bb2f50ac04eb27657f6a7396fd93c887
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2024-05-31 05:33:53 +0530

    meta tag google veri

--- app/layout.js
+++ app/layout.js
@@ -9,14 +9,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "NAS Js",
   description: "this is a webapplication to use NAS Js",
-  keywords : "NAS Js , NAS Js Home Page , NAS Js Get Started",
-  Author : "Raunak Singh"
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="ThZA6NtQKHBSbro3mSqr-Exr8f9wFwNmdrN9OMqLiAg" />
         <link rel="icon" href="./favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
